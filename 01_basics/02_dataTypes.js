7ae6c7030ea9ddf70e119d19da2d8bb32458031a
wasim-momin <40685269+wasim-momin@users.noreply.github.com>
2025-07-02 04:23:03 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+let name = "wasim"
+let age = 32
+let isLoggedIn = true
+let state
+
+//data types
+// number 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone vale and null is a object 
+// undefine => is a if self undefine
+// symbol => mostly used in react, figma to show uniqness
+// object
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
