225f7195eff30e173673bbd79fa1fc6c3f653bb5
wasim-momin <40685269+wasim-momin@users.noreply.github.com>
2025-07-02 04:22:51 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1001;
+let accountEmail = "wasim@gmail.com";
+var accountPassword = "12345";
+accountCity = "Pune"
+let accountState;
+
+//accountId = 2;
+
+accountEmail = "aa@gmail.com";
+accountPassword = "2222";
+accountCity = "Bara dddd"
+accountState = accountCity;
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
