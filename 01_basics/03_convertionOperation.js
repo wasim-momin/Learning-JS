3855f5b814734147ca6a6531e61e6cdb112e1182
wasim-momin <40685269+wasim-momin@users.noreply.github.com>
2025-07-02 05:16:18 +0000

    convertion operation

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,63 @@
+//let score = "33"
+//let score = "33aaa"
+//let score = null
+//let score = undefined
+//let score = true
+let score = "sssss"
+
+//console.log(typeof score);
+
+let scoreInNumber = Number(score)
+
+//console.log(typeof scoreInNumber);
+//console.log(scoreInNumber);
+
+let loggedIn = ""
+let booleanLoggedIn = Boolean(loggedIn)
+
+//console.log(booleanLoggedIn)
+
+
+let someNumber = true
+let convertString = String(someNumber)
+
+console.log(convertString);
+console.log(typeof convertString);
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
