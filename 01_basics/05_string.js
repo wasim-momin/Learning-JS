02cbed265ed4c3fc65472f2b8cd12d212a147fd6
wasim-momin <40685269+wasim-momin@users.noreply.github.com>
2025-07-04 12:14:08 +0000

    examples stirng

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -4,7 +4,7 @@ const repoCount = 456
 // console.log(typeof repoCount);
 //console.log(`My name is ${userName.toUpperCase()} and my repositry count ${repoCount}`);
 
-const projectURL = new String('wasim-momin.com')
+const projectURL = new String('wAsim-momin.com')
 
 // console.log(projectURL)
 //console.log(projectURL[0]);
@@ -13,7 +13,7 @@ const projectURL = new String('wasim-momin.com')
 // const projectURLLength = projectURL.length <= 3;
 // console.log(projectURLLength);
 
-console.log(projectURL.length);
+//console.log(projectURL.length);
 // console.log(projectURL.charAt(3));
 // console.log(projectURL.indexOf('s'));
 
@@ -23,6 +23,20 @@ console.log(projectURL.length);
 // console.log(userName)
 // console.log(userName.trim())
 
-console.log(projectURL.split('-'));
-console.log(projectURL.includes('0'));
+// console.log(projectURL.split('-'));
+// console.log(projectURL.includes('0'));
+
+// console.log(projectURL.at(4));
+
+console.log(projectURL.charAt(0));
+console.log(projectURL.charCodeAt(1));
+
+// charCodeAt print the ascii value of character
+const regexp = /t(e)(st(\d?))/g;
+const str ="test1test2"
+const resul = [...str.matchAll(regexp)]
+console.log(resul[0]);
+console.log(resul[1]);
+console.log('3',resul[3]);
+
 
