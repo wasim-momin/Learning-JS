e8443cb546f5f66c1f4530fd565979f9e613849f
wasim-momin <40685269+wasim-momin@users.noreply.github.com>
2025-07-04 11:09:20 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,28 @@
+const userName = "   Wasim    "
+const repoCount = 456
+
+// console.log(typeof repoCount);
+//console.log(`My name is ${userName.toUpperCase()} and my repositry count ${repoCount}`);
+
+const projectURL = new String('wasim-momin.com')
+
+// console.log(projectURL)
+//console.log(projectURL[0]);
+//console.log(projectURL.__proto__);
+
+// const projectURLLength = projectURL.length <= 3;
+// console.log(projectURLLength);
+
+console.log(projectURL.length);
+// console.log(projectURL.charAt(3));
+// console.log(projectURL.indexOf('s'));
+
+// console.log(projectURL.substring(0,3));
+// console.log(projectURL.slice(-16, 3));
+
+// console.log(userName)
+// console.log(userName.trim())
+
+console.log(projectURL.split('-'));
+console.log(projectURL.includes('0'));
+
