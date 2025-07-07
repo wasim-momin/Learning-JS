231cf335f89bc3df0e451c54892647131a09c34a
wasim-momin <40685269+wasim-momin@users.noreply.github.com>
2025-07-07 06:05:53 +0000

    controls

---
+++ 04_controls/switch.js
@@ -0,0 +1,27 @@
+let fruit = "banana";
+//withou break
+// switch (fruit) {
+//   case "apple":
+//     console.log("It's an apple");
+//   case "banana":
+//     console.log("It's a banana");
+//   case "orange":
+//     console.log("It's an orange");
+//   default:
+//     console.log("Unknown fruit");
+// }
+
+// with break
+switch (fruit) {
+  case "apple":
+    console.log("It's an apple");
+    break;
+  case "banana":
+    console.log("It's a banana");
+    break;
+  case "orange":
+    console.log("It's an orange");
+    break;
+  default:
+    console.log("Unknown fruit");
+}
---
+++ 04_controls/truecy.js
@@ -0,0 +1,44 @@
+const userName = null
+if(userName){
+    console.log('user name');
+}
+else{
+    console.log("not user name");
+}
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const userObj = {}
+if (Object.keys(userObj).length == 0){
+    console.log("name is empty");
+}
+else{
+    console.log("else part");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
