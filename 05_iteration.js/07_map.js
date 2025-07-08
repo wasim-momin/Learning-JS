d3704fef068c37f67cbc1478446c752d4f748fed
wasim-momin <40685269+wasim-momin@users.noreply.github.com>
2025-07-08 14:17:52 +0000

    array map

---
+++ 05_iteration.js/07_map.js
@@ -0,0 +1,12 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const add = myNumers.map((num)=>(num+9))
+console.log(add);
+
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
