66d3cacabc06c320438accc1e57cc65739bd8e5b
wasim-momin <40685269+wasim-momin@users.noreply.github.com>
2025-07-08 13:37:24 +0000

    array for each

---
+++ 05_iteration.js/05_for-each.js
@@ -0,0 +1,42 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+coding.forEach(function (val){
+   // console.log(val);
+});
+coding.forEach((val) => {
+     //console.log(val);
+});
+
+// using function
+function printMe (val){
+     //console.log(val);
+}
+coding.forEach(printMe) // function ka refe dena hai excute nai karna hai like printMe()
+
+coding.forEach((item, index, arr)=>{
+    //  console.log(item)
+    //  console.log(index)
+    //  console.log(arr)
+})
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+myCoding.forEach((item, index, arr)=>{
+     //console.log(item.languageFileName)
+     if (index === 0){
+        console.log(`on 1st position lang name is ${item.languageName}`)
+     }
+     
+    //  console.log(arr)
+})
